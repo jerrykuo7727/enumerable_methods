8e4c52e1b1a9950bc8266e8524cabc0407c9ab88
jerrykuo7727 <jerrykuo7727@gmail.com>
2016-08-02 22:52:31 +0800

    Add rspec test for #my_each_with_index

--- spec/enumerable_methods_spec.rb
+++ spec/enumerable_methods_spec.rb
@@ -1,10 +1,11 @@
 require 'enumerable_methods'
 
 describe "Enumerable" do
+  let(:arr) { [2, 4, 6] }
+  let(:hash) { {wow: 'cool', waw: 2} }
 
   describe "#my_each" do
-    let(:arr) { [2, 4, 6] }
-    let(:hash) { {wow: 'cool', waw: 2} }
+    
 
     context "given [2,4,6]" do
       it "sums up to 12" do
@@ -31,6 +32,25 @@ describe "Enumerable" do
         expect(values).to eql(['cool', 2])
       end
     end
+  end
 
+  describe "#my_each_with_index" do
+    context "given [2,4,6]" do
+      it "sums up to 12" do
+        sum = 0
+        arr.my_each_with_index { |e| sum += e }
+        expect(sum).to eql(12)
+      end
+      it "multiples up to 48" do
+        multiplication = 1
+        arr.my_each_with_index { |e| multiplication *= e }
+        expect(multiplication).to eql(48)
+      end
+      it "returns [0,1,2] as index" do
+        indices = []
+        arr.my_each_with_index { |e, i| indices << i }
+        expect(indices).to eql([0, 1, 2])
+      end
+    end
   end
 end
